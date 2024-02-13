36e51271c295a82fe4c82a0f3d40c8ae17cd21ad
Kaung Maw <kaungmawaung9999@gmail.com>
2024-02-13 10:41:32 +0630

    delete product completed

--- admin/product/DeleteProduct.php
+++ admin/product/DeleteProduct.php
@@ -4,10 +4,17 @@ include_once("../../database/TableNames.php");
 
 $product_id = $_GET['id'];
 
-$delete_category = "DELETE FROM $product_table WHERE id=$product_id";
+$delete_product_tag = "DELETE FROM $product_tag_table WHERE product_id=$product_id";
+try {
+    $pdo->exec($delete_product_tag);
+} catch (PDOException $pde) {
+    echo ($pde->getMessage());
+}
+
+$delete_product = "DELETE FROM $product_table WHERE id=$product_id";
 
 try {
-    $pdo->exec($delete_category);
+    $pdo->exec($delete_product);
     header("Location: ViewProducts.php");
 } catch (PDOException $pde) {
     echo $pde->getMessage();
